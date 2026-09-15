365113efc87a8a2a5c65d23ed0b8f99a695e8585
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-15 09:39:14 -0300

    Delete advancedmath.py

--- advancedmath.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if type(n) != int:
-        raise TypeError("The value of 'n' must be a integer number.")
-    if n < 0:
-        raise ValueError("The value of 'n' must be a non-negative integer.")
-    elif n == 0 or n == 1: 
-        return 1
-    else:
-        return n * factorial(n-1)
