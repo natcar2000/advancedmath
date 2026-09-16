8d1e5d3711d15e5f628323063f51f7dd0b79f6fd
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-16 10:28:41 -0300

    Refactor error messages in factorial and permutation

--- advancedmath/combinatorics.py
+++ advancedmath/combinatorics.py
@@ -1,9 +1,28 @@
+from .exceptions import InvalidPermutationError
+
+
 def factorial(n):
     if type(n) != int:
-        raise TypeError("The value of 'n' must be a integer number.")
+        raise TypeError("Value of 'n' must be a integer number.")
+        
     if n < 0:
-        raise ValueError("The value of 'n' must be a non-negative integer.")
+        raise ValueError("Value of 'n' must be a non-negative integer.")        
     elif n == 0 or n == 1: 
-        return 1
+        return 1        
     else:
         return n * factorial(n-1)
+
+
+def permutation(n, k):    
+    if type(k) != int:
+        raise TypeError("Value of 'k' must be a integer number.")
+
+    if k > n:
+        raise InvalidPermutationError("Value of 'k' cannot be greater than 'n'.")
+
+    if k < 0:
+        raise ValueError("Value of 'k' must be a non-negative integer.")
+    
+    fac1 = factorial(n)       
+    fac2 = factorial(n-k)
+    return int(fac1 / fac2)    
