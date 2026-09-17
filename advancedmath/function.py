0cb4a72ababe53199d1993481d2a360b77c6432c
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 11:27:23 -0300

    Fix calculation method for square root approximation

--- advancedmath/function.py
+++ advancedmath/function.py
@@ -47,8 +47,8 @@ def calculate_square_root(discriminant):
         return 0
 
     else:
-        guess = number
-        for value in range(20):
-            guess = (guess + value / guess) / 2
+        guess = discriminant
+        for _ in range(20):
+            guess = (guess + discriminant / guess) / 2
 
         return guess
