e62f31d1569d5af766ac8994cf4f477e6c47ceca
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 10:57:27 -0300

    Update function.py

--- advancedmath/function.py
+++ advancedmath/function.py
@@ -1,1 +1,8 @@
+def linear_function(a, b, x):
+    if a == 0:
+        raise ValueError("Coeficient 'a' must nob be 0.")
 
+    if type(a) not in (int, float) or type(b) not in (int, float) or type(x) not in (int, float):
+        raise TypeError("Numbers must have integer or float values.")
+
+    return (a * x) + b
