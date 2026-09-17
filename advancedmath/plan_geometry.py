69c5a1c91a5884440cdd4854c36479ada4c9c117
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 09:55:03 -0300

    Update plan_geometry.py

--- advancedmath/plan_geometry.py
+++ advancedmath/plan_geometry.py
@@ -63,18 +63,85 @@ class Parallelogram(Shape):
         return self.base * self.height
 
 
-class Circumference(Shape):
-    def __init__(self, radius):
+class Diamond(Shape):
+    def __init__(self, larger_diagonal, smaller_diagonal):
+        super().__init__(4)
+
+        self.validate_measure(larger_diagonal)
+        self.validate_measure(smaller_diagonal)
+
+        self.larger_diagonal = larger_diagonal
+        self.smaller_diagonal = smaller_diagonal
+
+    def area(self):
+        return (self.larger_diagonal * self.smaller_diagonal) / 2
+
+
+class Trapezoid(Shape):
+    def __init__(self, larger_base, smaller_base, height):
+        super().__init__(4)
+
+        self.validate_measure(larger_base)
+        self.validate_measure(smaller_base)
+        self.validate_measure(height)
+
+        self.larger_base = larger_base
+        self.smaller_base = smaller_base
+        self.height = height
+
+    def area(self):
+        return ((self.larger_base + self.smaller_base) * self.height) / 2 
+
+
+class Triangle(Shape):
+    def __init__(self, a, b, c, height):
+        self.validate_measure(a)
+        self.validate_measure(b)
+        self.validate_measure(c)
+        self.validate_measure(height)
+        
+        if not (a + b > c and a + c > b and b + c > a):
+            raise ValueError("The measures cannot form a triangle.")
+        
+        super().__init__(3)
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self.height = height
+   
+    def area(self):
+        return(self.a * self.height) / 2
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    def is_equilateral(self):
+        return self.a == self.b == self.c
+
+    def is_isosceles(self):
+        return self.a == self.b or self.a == self.c or self.b == self.c
+
+    def is_scalene(self):
+        return self.a != self.b and self.a != self.c and self.b != self.c
+
+    def is_right(self):
+        sides = sorted([self.a, self.b, self.c]) 
+        return sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2
+
+
+class Circle(Shape):
+    def __init__(self, circumference_radius):
         super().__init__(1)
 
         self.validate_measure(radius)        
-        self.radius = radius
+        self.circumference_radius = circumference_radius
 
     def area(self):
         return PI * (self.radius ** 2) 
 
-    def diameter(self):
+    def circumference_diameter(self):
         return 2 * self.radius
     
     def circumference_length(self):
-        return self.diameter() * PI
+        return self.circumference_diameter() * PI
