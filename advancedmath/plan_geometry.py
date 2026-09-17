fd97a044cec9c8ae6772e7639d9b2c63f646999b
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-16 22:05:15 -0300

    Update plan_geometry.py

--- advancedmath/plan_geometry.py
+++ advancedmath/plan_geometry.py
@@ -1,6 +1,12 @@
 class Polygon:
-    def __init__(self, sides):
-        self.sides = sides
+    def __init__(self, measures):
+        if type(measures) != int:
+            raise TypeError("Measures must be an integer.")
+
+        if measures < 3:
+            raise ValueError("A polygon must have at least 3 measures.")
+            
+        self.measures = measures
 
     def area(self):
         raise NotImplementedError
@@ -12,7 +18,14 @@ class Polygon:
 class Rectangle(Polygon):
     def __init__(self, width, height):
         super().__init__(4)
-        self.widht = widht
+
+        if type(width) not in (int, float) or type(height) not in (int, float):
+            raise TypeError("Measures must be integer or float values.")
+        
+        if width <= 0 or height <= 0:
+            raise ValueError("Measures must have positive values.")
+        
+        self.width = width
         self.height = height
 
     def area(self):
