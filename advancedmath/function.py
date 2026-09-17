614463fdd85fe75a0a88c4f79112b00b73a185c3
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 11:13:39 -0300

    Update function.py

--- advancedmath/function.py
+++ advancedmath/function.py
@@ -1,8 +1,37 @@
-def linear_function(a, b, x):
-    if type(a) not in (int, float) or type(b) not in (int, float) or type(x) not in (int, float):
-        raise TypeError("Numbers must have integer or float values.")
-    
-    if a == 0:
-        raise ValueError("Coefficient 'a' must not be 0.")
+def valida_valores(*valores):
+    for valor in valores:
+        if type(valor) not in (int, float):
+            raise TypeError("Numbers must have integer or float values.")
 
+
+def valida_coeficiente(coeficiente):
+    if coeficiente == 0:
+        raise ValueError("Coefficient 'a' must not be 0.")
+    
+
+def linear_function(a, b, x):    
+    valida_valores(a, b, x)
+    valida_coeficiente(a)
+    
     return (a * x) + b
+
+
+def linear_root(a, b):
+    valida_valores(a, b)
+    valida_coeficiente(a)
+
+    return -b / a
+
+
+def quadratic_function(a, b, c, x):
+    valida_valores(a, b, c, x)
+    valida_coeficiente(a)
+
+    return (a * (x ** 2)) + (b * x) + c
+
+
+def calculate_discriminant(a, b, c):
+    valida_valores(a, b, c)
+    valida_coeficiente(a)
+
+    return (b ** 2) - (4 * a * c)
