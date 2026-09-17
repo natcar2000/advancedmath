dc7919149214104f4f1e3a8c5bb3d308c35f57cd
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 15:44:17 -0300

    Update function.py

--- advancedmath/function.py
+++ advancedmath/function.py
@@ -81,10 +81,26 @@ def exponential_function(a, x, start=None):
     return start * (a ** x)
 
 
-def logarithmic_function_1(base, x, medium=None, lower=None, upper=None, precision=1e-10):
+def main_logarithmic_function(base, x,medium=None, lower=None, upper=None, precision=1e-10)
     validate_values(base, x)
     validate_coefficient(base)
+
+    if base == 1:
+        raise ValueError("Value of base cannot be 1.")
+        
+    if x <= 0:
+        raise ValueError("Value of x must be greater than 0.")
+    elif x == 1:
+        return 0
+    else:
+        if x % 1 == 0:
+            return logarithmic_function_1(base, x, medium=None, lower=None, upper=None, precision=1e-10)
+        logarithmic_function_2(base, x, medium=None, lower=None, upper=None, precision=1e-10)
+
+    return logarithmic_function_2(base, x, medium=None, lower=None, upper=None, precision=1e-10)
     
+
+def logarithmic_function_1(base, x, medium=None, lower=None, upper=None, precision=1e-10):
     if base == 1:
         raise ValueError("Value of base cannot be 1.")
 
@@ -113,9 +129,6 @@ def logarithmic_function_1(base, x, medium=None, lower=None, upper=None, precisi
 
 
 def logarithmic_function_2(base, x, medium=None, lower=None, upper=None, precision=1e-10):    
-    validate_values(base, x)
-    validate_coefficient(base)
-    
     integer = int(x)
     
     for value in range(integer):
@@ -139,9 +152,34 @@ def logarithmic_function_2(base, x, medium=None, lower=None, upper=None, precisi
     return logarithmic_function_2(base, x, medium, lower, upper, precision)
 
 
-def logarithmic_function_3(base, x, medium=None, lower=None, upper=None, precision=1e-10):    
-    validate_values(base, x)
-    validate_coefficient(base)
-
+def logarithmic_function_3(base, x, medium=None, lower=None, upper=None, precision=1e-10):   
     result = logarithmic_function_2(base, 1/x)
     return -result
+    
+    
+def logarithmic_function_1(base, x, medium=None, lower=None, upper=None, precision=1e-10):
+    if base == 1:
+        raise ValueError("Value of base cannot be 1.")
+
+    for value in range(x):
+        if base ** value <= x:
+            integer = value
+            
+    if base ** integer == x:
+        return integer
+
+    if lower is None and upper is None:
+        lower = integer
+        upper = integer+1
+
+    medium = (lower+upper) / 2
+    
+    if base ** medium < x:
+        lower = medium
+    else:
+        upper = medium
+        
+    if abs((base ** medium) - x) < precision:
+        return medium
+        
+    return logarithmic_function_1(base, x, medium, lower, upper, precision)
