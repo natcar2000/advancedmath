4e98011b01c2d56ace99652433647c2ff89537e2
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-17 10:58:54 -0300

    Fix error message for zero coefficient in linear function

--- advancedmath/function.py
+++ advancedmath/function.py
@@ -1,8 +1,8 @@
 def linear_function(a, b, x):
-    if a == 0:
-        raise ValueError("Coeficient 'a' must nob be 0.")
-
     if type(a) not in (int, float) or type(b) not in (int, float) or type(x) not in (int, float):
         raise TypeError("Numbers must have integer or float values.")
+    
+    if a == 0:
+        raise ValueError("Coefficient 'a' must not be 0.")
 
     return (a * x) + b
