0ef77e1485f901f061af94ca71928018e3bea9b4
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-18 08:31:04 -0300

    Update combinatorics.py

--- advancedmath/combinatorics.py
+++ advancedmath/combinatorics.py
@@ -10,8 +10,31 @@ def factorial(n):
         return n * factorial(n-1)
 
 
+def validate_values(n, k):
+    if k > n:
+        raise ValueError("Value of k must be smaller than n.")
+        
+    return True
+
+
 def permutation(n):
     return factorial(n)
+
+
+def permutation_with_repetition(n, *repetitions):
+    denominator = 1
+    soma = 0
+    
+    for repetition in repetitions:
+        validate_values(n, k)
+        fac = factorial(repetition)
+        denominator *= fac
+        soma += repetition
+
+    if soma > n:
+        raise ValueError("The sum of the repetitions can't be greater than n.")
+    
+    return factorial(n) // denominator
 
 
 def circular_permutation(n):
@@ -19,12 +42,15 @@ def circular_permutation(n):
 
 
 def arrangement(n, k):   
+    validate_subset(n, k)
     return factorial(n) // factorial(n-k)
 
 
 def combination(n, k):
+    validate_subset(n, k)
     return factorial(n) // (factorial(k) * factorial(n-k))
 
 
 def combination_with_repetition(n, k):
+    validate_subset(n, k)
     return combination(n+k-1, k)
