3020bffd41f50905281c2c3908f68638d7a15669
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-18 09:55:59 -0300

    Add midpoint function to calculate midpoint between points

--- advancedmath/analytic_geometry.py
+++ advancedmath/analytic_geometry.py
@@ -6,3 +6,9 @@ def distance_between_points(x1, y1, x2, y2):
     validate_values(x1, y1, x2, y2)
     value = ((x2-x1) ** 2) + ((y2-y1) ** 2)
     return calculate_square_root(value)
+
+
+def midpoint(x1, y1, x2, y2):
+    validate_values(x1, y1, x2, y2)
+    x, y = (x1+x2)/2, (y1+y2)/2
+    return x, y
