1c85f5188f77b2395f09581bf0f639fab30072d6
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-18 09:46:01 -0300

    Fix argument order in validate_values call

--- advancedmath/analytic_geometry.py
+++ advancedmath/analytic_geometry.py
@@ -3,6 +3,6 @@ from function import validate_values
 
 
 def distance_between_points(x1, y1, x2, y2):
-    validate_values(x1, x2, y1, y2)
+    validate_values(x1, y1, x2, y2)
     value = ((x2-x1) ** 2) + ((y2-y1) ** 2)
     return calculate_square_root(value)
