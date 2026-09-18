aa22a12c0f4b2a1e0def9a1cfe14f831961684f2
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-18 10:39:38 -0300

    Update statistic.py

--- advancedmath/statistic.py
+++ advancedmath/statistic.py
@@ -2,7 +2,11 @@ from function import calculate_square_root
 from function import validate_values
 
 
-def media(*roll):
+def mean(*roll):
     total = 0
+    
     for data in roll:
-        
+        validate_values(data)
+        total += data
+    
+    return total / len(roll)
