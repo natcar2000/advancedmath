41d3b75749874261297337bd4ecbc43e7d0e7038
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-18 11:01:35 -0300

    Update statistic.py

--- advancedmath/statistic.py
+++ advancedmath/statistic.py
@@ -2,11 +2,29 @@ from function import calculate_square_root
 from function import validate_values
 
 
-def mean(*roll):
+def arithmetic_mean(*values):
     total = 0
     
-    for data in roll:
-        validate_values(data)
-        total += data
+    for value in values:
+        validate_values(value)
+        total += value
     
-    return total / len(roll)
+    return total / len(values)
+
+
+def weighted_mean(weights, *values):
+    if len(weights) != len(values):
+        raise ValueError("Weights and values data must have the same length.")
+    
+    total = 0
+    numerator = 0
+    
+    for index in range(len(weights)):
+        validate_values(weights[index], values[index])
+        numerator += weights[index] * values[index]
+        total += weights[index]
+    
+    if total == 0:
+        raise ValueError("The total of weights must be different of zero.")
+        
+    return numerator / total
